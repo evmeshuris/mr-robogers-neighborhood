322764ba911d6dc8a612a31e443b9ba346aa3524
evmeshuris <101578340+evmeshuris@users.noreply.github.com>
2022-04-01 16:30:12 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
-// Utility Logic
-
 function noInputtedNumber(passage) {
   if (passage.trim().lenght === 0) {
     return alert("Please enter the number");
   }
 }
-// Business Logic
 
-//get array from a number input// input 5 - [0,1,2,3,4,5]
 function arrayOfNumbers(passage) {
 const numberInput = passage;
 let arrayNumber = [];
@@ -30,34 +26,8 @@ let arrayNumber = [];
     }
 
   });
-  // var mixArray = totalMix.join();
-  // var mixString = mixArray[0].split(',');
-  // mixString = mixString.map(i => '"' + i + '"');
-  return totalMix;
+    return totalMix;
 }
- // loop checking 1, 2 ,3, replace with words// 
-// function subNumWithWords() {
-// const mixArray = arrayOfNumbers(arrayNumber);
-// let totalMix = [];
-// mixArray.forEach(function(digit) {
-//     if ((digit.lenght -1) === 1) {
-//       totalMix.push("Beep!");
-//     }
-//     else if ((digit.lenght -1) === 2) {
-//       totalMix.push("Boop!");
-//     }
-//     else if ((digit.lenght -1) === 3) {
-//       totalMix.push("Won't you be my neighbor?");
-//     }
-//     else {
-//       totalMix.push(digit);
-//     }
-//   return totalMix;
-//   });
-// }
-
-//UI Logic
-
 
 $(document).ready(function () {
   $("form#word-counter").submit(function (event) {
