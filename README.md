4f53c801157d8d485c52a953b68e295157197f62
Evgeniya Meshuris <em@theemm.com>
2022-04-01 14:49:41 -0700

    add README.md

---
+++ README.md
@@ -0,0 +1,71 @@
+# _Programming Language Suggester_
+
+#### By _**Evgeniya Meshuris**_
+
+#### _This is programming language suggester will give a result of what language you should learn based on your answers._
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _JavaScript_
+* _jQuery_
+
+## Description
+
+_This is programming language suggester will give a result of what language you should learn based on your experience, age and occupation._
+
+## Setup/Installation Requirements
+
+* _Copy the repository URL named "mr-robogers-neighborhood"_
+* _Clone the copied repository to your computer_
+* _Open the cloned directory named "mr-robogers-neighborhood"_
+* _From the top of this directory, open index.html_
+* _Fill out the From_
+* _Click Submit to see the result_
+* _Click Submit again if you want to repeat a request_
+
+## Known Bugs
+
+* _None known bugs_
+
+
+## License
+
+Licensed under the [MIT License](LICENSE)
+
+Copyright (c) _2022_ _Evgeniya Meshuris_
+
+// *********************************************************************************************** \\
+
+Specs
+
+Return an Alert("Enter the number") if no number was given
+Count up to entered number
+Check entered numbers if they end with 3, if true return "Won't you be my neighbor?"
+Check entered numbers if they end with 2, if true return "Boop!"
+Check entered numbers if they end with 1, of true return "Beep!"
+Return string mixString
+
+Test: "If number not given, return "Please enter a number"" 
+Code: noInputtedNumber() ;
+Expected output: "Please enter a number";
+
+Test: "When a number is entered, the array will count up to that number" 
+Code: counter("5"); 
+Expected output: ["0","1", "2", "3", "4", "5"];
+
+arrayOfNumbers()
+
+Test: "Check entered numbers if they end with 3, if true return "Won't you be my neighbor?" 
+Code: arrayOfNumbers("23"); 
+Expected output: ["Won't you be my neighbor?"]
+
+Test: "Check entered numbers if they end with 2, if true return "Boop!" 
+Code: arrayOfNumbers("12"); 
+Expected output: ["boop"];
+
+Test: "Check entered numbers if they end with 1, of true return "Beep!" 
+Code: arrayOfNumbers("1"); 
+Expected output: ["Beep!"];
+
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 // Utility Logic
 
 function noInputtedNumber(passage) {
-  if (passage === 0) {
-  return alert("Enter the number");
+  if (passage.trim().lenght === 0) {
+    return alert("Please enter the number");
   }
 }
 // Business Logic
@@ -12,30 +12,49 @@ function arrayOfNumbers(passage) {
 const numberInput = passage;
 let arrayNumber = [];
   for (var number = 0; number <= numberInput; number += 1) {
-    arrayNumber.push(parseInt(number));
+    arrayNumber.push(number.toString());
   }
-  return arrayNumber;
-}
- // loop checking 1, 2 ,3, replace with words// 
-function subNumWithWords() {
-const mixArray = arrayOfNumbers(arrayNumber);
-let totalMix = [];
-mixArray.forEach(function(digit) {
-    if ((digit.lenght -1) === 1) {
+  let totalMix = [];
+  arrayNumber.forEach(function(digit) {
+    if (String(digit).slice(-1) === '1') {
       totalMix.push("Beep!");
     }
-    else if ((digit.lenght -1) === 2) {
+    else if (String(digit).slice(-1) === '2') {
       totalMix.push("Boop!");
     }
-    else if ((digit.lenght -1) === 3) {
+    else if (String(digit).slice(-1) === '3') {
       totalMix.push("Won't you be my neighbor?");
     }
     else {
-      totalMix.push(digit);
+      totalMix.push(digit + "");
     }
-  return totalMix;
+
   });
+  // var mixArray = totalMix.join();
+  // var mixString = mixArray[0].split(',');
+  // mixString = mixString.map(i => '"' + i + '"');
+  return totalMix;
 }
+ // loop checking 1, 2 ,3, replace with words// 
+// function subNumWithWords() {
+// const mixArray = arrayOfNumbers(arrayNumber);
+// let totalMix = [];
+// mixArray.forEach(function(digit) {
+//     if ((digit.lenght -1) === 1) {
+//       totalMix.push("Beep!");
+//     }
+//     else if ((digit.lenght -1) === 2) {
+//       totalMix.push("Boop!");
+//     }
+//     else if ((digit.lenght -1) === 3) {
+//       totalMix.push("Won't you be my neighbor?");
+//     }
+//     else {
+//       totalMix.push(digit);
+//     }
+//   return totalMix;
+//   });
+// }
 
 //UI Logic
 
@@ -44,7 +63,7 @@ $(document).ready(function () {
   $("form#word-counter").submit(function (event) {
     event.preventDefault();
     const passage = $("#number-passage").val();
-    const totalMix = subNumWithWords(passage);
+    const totalMix = arrayOfNumbers(passage);
     $("#total-mix").html(totalMix);
   });
 });
